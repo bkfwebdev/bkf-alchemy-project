c6125195ab2ad8390982d965be1ef0d708125b79
bkfwebdev <bryant.feld@gmail.com>
2021-08-26 22:12:26 -0400

    forgot to import Post from models...duh

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, redirect, render_template
 from flask_debugtoolbar import DebugToolbarExtension
-from models import db, connect_db, User
+from models import db, connect_db, User, Post
 
 app = Flask(__name__)
 # app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///blogly" - did not work
